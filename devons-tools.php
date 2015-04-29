f3faeb002006bdc74ce5162468c9ab201a4cd32c
devonhg <devon.hugh.godfrey@gmail.com>
2015-04-29 14:45:25 -0400

    Updated version number

--- devons-tools.php
+++ devons-tools.php
@@ -4,19 +4,17 @@ if ( ! defined( 'WPINC' ) ) { die; }
  * Plugin Name:       Devons Tools - Post-Type API
  * Plugin URI:        
  * Description:       This is the Core for Devons Tools Framework
- * Version:           1.3
+ * Version:           0.9
  * Author:            Devon Godfrey
  * Author URI:        http://playfreygames.net
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
 
-	This plugin is based on Devons Tools Beta 1.3
+	This plugin is based on Devons Tools Beta 0.9
 
 	*IMPORTANT*
 	do a "find/replace" accross the directory for "MYPLUGIN" and replace
 	with your plugin name. 
-
-	Any files put into the inc folder automatically get included. 
 */
 
 //Include the core class of the post type api
