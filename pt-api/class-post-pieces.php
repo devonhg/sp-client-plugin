fbeb5024074bbb565ee4986e78a07ba3974776f6
devonhg <devon.hugh.godfrey@gmail.com>
2015-05-04 09:54:28 -0400

    Cleaned up the code a little

--- pt-api/class-post-pieces.php
+++ pt-api/class-post-pieces.php
@@ -11,23 +11,21 @@ class MYPLUGIN_pt_pcs{
 
 		//Title
 		public static function pt_title( $quer = null ){
-			if (!$quer == null){ $post = $quer; }
-			else{ global $post; }
+			$id = MYPLUGIN_func::get_pieces_id( $quer );
 
 			$out = "";
-	    	$out .= "<h1>" . $post->post_title . "</h1>";
+	    	$out .= "<h1>" . get_the_title( $id ) . "</h1>";
 
 	    	echo $out; 
 		}
 
 		//Content
 		public static function pt_content( $quer = null ){
-			if (!$quer == null){ $post = $quer; }
-			else{ global $post; }
+			$id = MYPLUGIN_func::get_pieces_id( $quer );
 
 			$out = "";
 	   		$out .= "<div class='" . "MYPLUGIN-content" . "'>";
-		    			$out .= get_the_content( $post->ID ); 
+		    			$out .= get_the_content( $id ); 
 		    $out .= "</div>";
 		
 			echo $out;
@@ -35,13 +33,12 @@ class MYPLUGIN_pt_pcs{
 
 		//Featured Image
 		public static function pt_fi( $quer = null ){
-			if (!$quer == null){ $post = $quer; }
-			else{ global $post; }
+			$id = MYPLUGIN_func::get_pieces_id( $quer );
 
 			$out = "";
-	    	if ( has_post_thumbnail( $post->ID ) ){
+	    	if ( has_post_thumbnail( $id ) ){
 		    	$out .= "<div class='" . "MYPLUGIN-image" . "'>";
-		    		$out .= "<a title='" . $post->post_title . "' href='" . get_permalink( $post->ID ) .  "' alt='" . $post->post_title . "'>" . get_the_post_thumbnail( $post->ID ) . "</a>"; 
+		    		$out .= "<a title='" . get_the_title( $id ) . "' href='" . get_permalink( $id ) .  "' alt='" . get_the_title( $id ) . "'>" . get_the_post_thumbnail( $id ) . "</a>"; 
 		    	$out .= "</div>";
 			}		
 
@@ -50,13 +47,12 @@ class MYPLUGIN_pt_pcs{
 
 		//Featured image, medium sized
 		public static function pt_fimed( $quer = null ){
-			if (!$quer == null){ $post = $quer; }
-			else{ global $post; }
+			$id = MYPLUGIN_func::get_pieces_id( $quer );
 
 			$out = "";
-	    	if ( has_post_thumbnail( $post->ID ) ){
+	    	if ( has_post_thumbnail( $id ) ){
 		    	$out .= "<div class='" . "MYPLUGIN-image" . "'>";
-		    		$out .= "<a title='" . get_the_title( $post->ID ) . "' href='" . get_permalink( $post->ID ) .  "' alt='" .  get_the_title( $post->ID ) . "'>" . get_the_post_thumbnail( $post->ID , "medium" ) . "</a>"; 
+		    		$out .= "<a title='" . get_the_title($id ) . "' href='" . get_permalink( $id ) .  "' alt='" .  get_the_title( $id ) . "'>" . get_the_post_thumbnail( $id , "medium" ) . "</a>"; 
 		    	$out .= "</div>";
 			}		
 
@@ -65,10 +61,7 @@ class MYPLUGIN_pt_pcs{
 
 		//Output unhidden meta
 		public static function pt_meta( $quer = null ){
-			//if (!$quer == null){ $post = $quer; }
-			//else{ global $post; }
 			$id = MYPLUGIN_func::get_pieces_id( $quer );
-
 
 			$out = "";
 	   		$out .= "<div class='" . "MYPLUGIN-meta" . "'>";
@@ -80,10 +73,7 @@ class MYPLUGIN_pt_pcs{
 
 		//Output unhidden media
 		public static function pt_media( $quer = null ){
-			//if (!$quer == null){ $post = $quer; }
-			//else{ global $post; }
 			$id = MYPLUGIN_func::get_pieces_id( $quer );
-
 
 			$out = "";
 	   		$out .= "<div class='" . "MYPLUGIN-media" . "'>";
@@ -95,14 +85,13 @@ class MYPLUGIN_pt_pcs{
 
 		//Output Categories
 		public static function pt_cats( $quer = null ){
-			if (!$quer == null){ $post = $quer; }
-			else{ global $post; }
+			$id = MYPLUGIN_func::get_pieces_id( $quer );
 
 			$out = "";
-			$pt_values = get_object_taxonomies( get_post_type( $post->ID ) );
+			$pt_values = get_object_taxonomies( get_post_type( $id ) );
 	    	$out .= "<div class='" . "MYPLUGIN-categories" . "'>";
 		    	foreach($pt_values as $tax){
-		    		$out .= MYPLUGIN_func::get_cats( $tax , $post->ID , $post->post_type );
+		    		$out .= MYPLUGIN_func::get_cats( $tax , $id , get_post_type( $id ) );
 		    	}
 	    	$out .= "</div>";	
 
@@ -112,12 +101,11 @@ class MYPLUGIN_pt_pcs{
 	//Archive Versions
 		//Title hyperlinked
 		public static function pt_title_a( $quer = null ){
-			if (!$quer == null){ $post = $quer; }
-			else{ global $post; }
+			$id = MYPLUGIN_func::get_pieces_id( $quer );
 
 			$out = "";
-			$out .= "<a title='" . get_the_title( $post->ID ) . "' href='" . get_permalink( $post->ID ) .  "'>";
-	    		$out .= "<h1>" . get_the_title( $post->ID ) . "</h1>";
+			$out .= "<a title='" . get_the_title( $id ) . "' href='" . get_permalink( $id ) .  "'>";
+	    		$out .= "<h1>" . get_the_title( $id ) . "</h1>";
 	    	$out .= "</a>";
 
 	    	echo $out; 
